d6310025a73c08d1f82ac2bb52f7c3804d45ca7d
vuxnq <vuxnqu@gmail.com>
2022-06-16 17:43:10 +0200

    added radiohead tab

--- script.js
+++ script.js
@@ -2,6 +2,7 @@
 document.getElementById("about-app").onclick = function () { page("about") };
 document.getElementById("projects-app").onclick = function () { page("projects") };
 
+document.getElementById("radiohead-link").onclick = function () { tab('radiohead') };
 document.getElementById("chliq-link").onclick = function () { tab('chliq') };
 document.getElementById("icons-link").onclick = function () { tab('icons') };
 document.getElementById("sweet-berries-link").onclick = function () { tab('sweet-berries') };
