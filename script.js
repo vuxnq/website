45f9e6106572c03195e99a06ddb97a02456a04ea
vuxnq <vuxnqu@gmail.com>
2022-10-16 13:58:13 +0200

    stylized lastfm widget

--- script.js
+++ script.js
@@ -37,13 +37,19 @@ function tab(selected) {
 }
 
 // last.fm nowplaying
-fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=vuxnq&api_key=70da5f72ca8ddc45015f96f2a0369819&limit=10&nowplaying=true&format=json')
+fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=vuxnq&api_key=70da5f72ca8ddc45015f96f2a0369819&limit=1&nowplaying=true&format=json')
   .then(res => res.json())
   .then(data => {
     var track = data.recenttracks.track[0]
+
+    document.getElementById('lastfm-image').innerHTML = "<img src=\"" + track.image[2]["#text"] + "\">";
+    document.getElementById('lastfm-track').innerHTML = track.name;
+    document.getElementById('lastfm-artist').innerHTML = track.artist["#text"];
+    document.getElementById("lastfm").style.display = "flex";
+
     if (track['@attr'] && track['@attr']['nowplaying']) {
-      document.getElementById('nowplaying').innerHTML = track.artist["#text"] + " - " + track.name;
+      document.getElementById("lastfm").className += "nowplaying";
     } else {
-      document.getElementById('nowplaying').innerHTML = "not listening to music rn";
+      document.getElementById("lastfm-status").innerHTML += "last played";
     }
   })
