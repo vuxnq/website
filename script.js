c2b26e96024e5adec76999f9d8162342bcde3668
vuxnq <vuxnqu@gmail.com>
2022-10-18 21:42:53 +0200

    auto-refresh of lastfm api, added game project

--- script.js
+++ script.js
@@ -1,13 +1,14 @@
-// Buttons Onclicks
+// buttons onclicks
 document.getElementById("about-app").onclick = function () { page("about") };
 document.getElementById("projects-app").onclick = function () { page("projects") };
 
 document.getElementById("radiohead-link").onclick = function () { tab('radiohead') };
+document.getElementById("game-link").onclick = function () { tab('game') };
 document.getElementById("chliq-link").onclick = function () { tab('chliq') };
 document.getElementById("icons-link").onclick = function () { tab('icons') };
 document.getElementById("sweet-berries-link").onclick = function () { tab('sweet-berries') };
 
-// Switching windows
+// switching windows
 function page(selected) {
   var page = document.getElementsByTagName('section');
   for (var i = 0; i < page.length; i++) {
@@ -21,7 +22,7 @@ function page(selected) {
   document.getElementById(selected + "-app").className += " checked";
 }
 
-// Switching tabs in projects
+// switching tabs in projects
 function tab(selected) {
   var content = document.getElementsByClassName("content");
   for (var i = 0; i < content.length; i++) {
@@ -35,23 +36,35 @@ function tab(selected) {
   document.getElementById(selected + "-link").className += " checked";
 }
 
-// last.fm nowplaying
-fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=vuxnq&api_key=70da5f72ca8ddc45015f96f2a0369819&limit=1&nowplaying=true&format=json')
-  .then(res => res.json())
-  .then(data => {
-    var track = data.recenttracks.track[0];
+// start on load
+window.onload = lastfm();
 
-    document.getElementById('lastfm-image').innerHTML = "<img height=\"85\" width=\"85\" src=\"" + track.image[2]["#text"] + "\">";
-    document.getElementById('lastfm-track').innerHTML = track.name;
-    document.getElementById('lastfm-artist').innerHTML = track.artist["#text"];
-    document.getElementById("lastfm").style.display = "flex";
+// automatic refresh of lastfm
+setInterval(function() {
+  lastfm();
+}, 5000);
 
-    if (track['@attr'] && track['@attr']['nowplaying']) {
-      document.getElementById("lastfm").className += "nowplaying";
-    } else {
-      document.getElementById("lastfm-status").innerHTML = timeSince(new Date(1000 * track.date.uts));
-    }
-  })
+// lastfm api
+function lastfm() {
+  fetch('https://ws.audioscrobbler.com/2.0/?method=user.getRecentTracks&user=vuxnq&api_key=70da5f72ca8ddc45015f96f2a0369819&limit=1&nowplaying=true&format=json')
+    .then(res => res.json())
+    .then(data => {
+      var track = data.recenttracks.track[0];
+
+      document.getElementById('lastfm-image').innerHTML = "<img height=\"85\" width=\"85\" src=\"" + track.image[2]["#text"] + "\">";
+      document.getElementById('lastfm-track').innerHTML = track.name;
+      document.getElementById('lastfm-artist').innerHTML = track.artist["#text"];
+      document.getElementById("lastfm").style.display = "flex";
+
+      if (track['@attr'] && track['@attr']['nowplaying']) {
+        document.getElementById("lastfm").className = "nowplaying";
+        document.getElementById("lastfm-status").innerHTML = null;
+      } else {
+        document.getElementById("lastfm").className = "";
+        document.getElementById("lastfm-status").innerHTML = timeSince(new Date(1000 * track.date.uts));
+      }
+    })
+}
 
 function timeSince(date) {
   var seconds = Math.floor((new Date() - date) / 1000);
